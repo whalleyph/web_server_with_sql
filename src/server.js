e4e39b9f44f4abf8631bd62bd7fa8aee1862fde4
2405-011-pw <walypa56@gmail.com>
2024-07-17 13:56:21 +0100

    setup db-check

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import { app } from "./support/setupExpress.js";
 import { query } from "./support/db.js";
-import { sum } from "./sum.js";
-import { setupARouteHandlerDemonstratingValidationWithZod } from "./zodDemo/setupARouteHandlerDemonstratingValidationWithZod.js";
+import pg from "pg";
 
-//You should delete all of these route handlers and replace them according to your own requirements
+//http request body
+const dbURL = process.env.DATABASE_URL;
+console.log(dbURL.length);
+
+const client = new pg.Client({
+    connectionString: dbURL,
+    ssl: true,
+});
+
+client.connect();
 
 app.get("/", (req, res) => {
     res.json({
@@ -12,32 +20,15 @@ app.get("/", (req, res) => {
     });
 });
 
-//just an example route handler.  delete it.
-app.get("/sum/:a/:b", handleGETRequestForSum);
-
-//This jsdoc comment helps vscode figure out the correct types for req and res for autocompletion, etc,
-//when it can't figure it out from context.
-/**
- * @param {import('express').Request} req
- * @param {import('express').Response} res
- */
-function handleGETRequestForSum(req, res) {
-    const answer = sum(parseInt(req.params.a), parseInt(req.params.b));
-    res.json({ answer });
-}
-
 //An example route that makes an SQL query to the db.
 app.get("/db-check", async (req, res) => {
     try {
-        const dbResult = await query("select * from my_table");
+        const dbResult = await query("select * from chat_messages");
         res.json(dbResult.rows);
     } catch (error) {
         console.error("error handling db-check: ", error);
     }
 });
-
-//Delete this, too.  It's just a demo for one way to robustly validate user-submitted data.
-setupARouteHandlerDemonstratingValidationWithZod(app);
 
 // use the environment variable PORT, or 4000 as a fallback
 const PORT = process.env.PORT ?? 4000;
