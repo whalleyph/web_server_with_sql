c8c23bedab34352343fafcc687877518e11a0860
2405-011-pw <walypa56@gmail.com>
2024-07-18 10:38:27 +0100

    delete useless files

--- src/sum.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "vitest";
-import { sum } from "./sum.js";
-
-test("sum", () => {
-    expect(sum(10, 20)).toBe(30);
-    expect(sum(0, 0)).toBe(0);
-    expect(sum(-20, 20)).toBe(0);
-});
